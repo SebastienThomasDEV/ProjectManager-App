9b72361551001f2a33fed22b8301b2a9ea9e16fe
Sébastien THOMAS <74055963+SebastienThomasDEV@users.noreply.github.com>
2022-11-25 10:44:59 +0100

    Setup project

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require 'vendor/autoload.php';
+use Team\Projectbuilder\Core\Dispatcher;
+
+new Dispatcher();
---
+++ src/Views/Template/Footer.php
@@ -0,0 +1,5 @@
+<footer>
+            Mon footer
+        </footer>   
+    </body>
+</html>
