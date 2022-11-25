ea6b0dc811bcf7fdbfbb8a7194b3421ac471b828
Ania <a.a.kuchcinska@gmail.com>
2022-11-25 16:31:03 +0100

    projectcontroller issue

--- src/Controller/DefaultPage.php
+++ src/Controller/DefaultPage.php
@@ -5,11 +5,11 @@ namespace Team\Projectbuilder\Controller;
 use Team\Projectbuilder\Core\Security;
 use Team\Projectbuilder\Core\Views;
 
-class DefaultPage
-{
+class DefaultPage{
+
     public function __construct()
     {
-        $view = new Views('DefaultPage', 'Ma super Appli');
+        $view = new Views('DefaultPage', 'Project Builder');
         if (isset($_POST['submit'])) {
             Security::ConnectUser();
         }
