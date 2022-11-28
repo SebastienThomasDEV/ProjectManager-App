1ae033b0206571b12a1fe0d3c6b25139b2b79100
NicolasSoulay <soulaynicolas@gmail.com>
2022-11-29 00:26:42 +0100

    la regrace

--- src/Views/CreateTask.php
+++ src/Views/CreateTask.php
@@ -1,9 +1,16 @@
+<?php
+
+
+if (isset($message)) {
+    echo '<div>' . $message . '</div>';
+}
+?>
+
+
 <form method="POST" action='index.php?page=<?php echo $_GET['page'] . "&idproject=" . $_GET['idproject'] . "&insert=1"; ?>'>
     <input name='title' type='text' placeholder='Add Task Name'>
     <input name='description' type='text' placeholder='Add Task Description'>
     <input name='priority' type='num' placeholder='Add Task Priority'>
-    <input name='lifecycle' type='text' placeholder='Add Task Lifecycle'>
-    <input name='idProject' type='text' value="<?php echo $_GET['idproject']; ?>" style="display:none">
-    <input name='idUser' type='text' value="NULL" style="display:none">
+    <input name='lifeCycle' type='text' placeholder='Add Task Lifecycle'>
     <input type='submit' name='create' value='<?php echo $submit; ?>'>
 </form>
