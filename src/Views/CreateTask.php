4633abffe67393d3b413de4c0599c3c792a94b88
Sébastien THOMAS <74055963+SebastienThomasDEV@users.noreply.github.com>
2022-11-30 14:46:28 +0100

    session et class isadmin

--- src/Views/CreateTask.php
+++ src/Views/CreateTask.php
@@ -7,10 +7,14 @@ if (isset($message)) {
 ?>
 
 
-<form method="POST" action=''>
-    <input name='title' type='text' placeholder='Add Task Name' value="<?php echo isset($title) ? $title: ''; ?>">
-    <input name='description' type='text' placeholder='Add Task Description' value="<?php echo isset($description) ? $description: ''; ?>">
-    <input name='priority' type='text' placeholder='Add Task Priority' value="<?php echo isset($priority) ? $priority: ''; ?>">
-    <input name='lifeCycle' type='text' placeholder='Add Task Lifecycle' value="<?php echo isset($lifeCycle) ? $lifeCycle: ''; ?>">
-    <input type='submit' name='create' value='<?php echo $submit; ?>'>
+<form method="POST" action='' id="taskform">
+    <input name='title' type='text' placeholder='Add Task Name' value="<?= isset($title) ? $title: ''; ?>">
+    <input name='description' type='text' placeholder='Add Task Description' value="<?= isset($description) ? $description: ''; ?>">
+    <input name='priority' type='text' placeholder='Add Task Priority' value="<?= isset($priority) ? $priority: ''; ?>">
+    <select name="lifeCycle" form="taskform">
+        <option value="started">Started</option>
+        <option value="inprogress">In progress</option>
+        <option value="finished">Finished</option>
+    </select>
+    <input type='submit' name='create' value='<?= $submit; ?>'>
 </form>
