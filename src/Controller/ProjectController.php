264e7f9be2fb1d0811359a0cb2d8c35b8c82ca4e
SebastienThomasDEV <sebastienthomaspr@gmail.com>
2022-12-01 02:05:06 +0100

    lots of change

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -10,23 +10,26 @@ use Team\Projectbuilder\Core\Security;
 use Team\Projectbuilder\Core\Views;
 use Team\Projectbuilder\Core\Validate;
 
-class ProjectController {
+class ProjectController
+{
 
-    public function __construct() {
-        if(isset($_GET['delete'])){
+    public function __construct()
+    {
+        if (isset($_GET['delete'])) {
             $this->deleteProject();
         }
-        
+
         if (isset($_GET['insert'])) {
             $this->createProject();
-        } elseif (isset($_GET['update'])){
+        } elseif (isset($_GET['update'])) {
             $this->updateProject();
         } else {
             $this->displayProject();
         }
     }
 
-    public function createProject () {
+    public function createProject()
+    {
         $view = new Views('CreateProject', 'Project creator');
         if (Security::isConnected()) {
             $view->setVar('connected', true);
@@ -35,12 +38,12 @@ class ProjectController {
         }
         $view->setVar('submit', 'Create project');
         $view->setVar('message', 'Create a new project');
-        $view->setVar('action','&insert=1');
+        $view->setVar('action', '&insert=1');
         if (isset($_POST['create'])) {
-            if (($message=$this->isValid()) === '') {
-                if(Project::create()) {
-                    $view->setVar('message','New project created successfully');
-                    $project=Project::getByAttribute('projectName',$_POST['projectName']);
+            if (($message = $this->isValid()) === '') {
+                if (Project::create()) {
+                    $view->setVar('message', 'New project created successfully');
+                    $project = Project::getByAttribute('projectName', $_POST['projectName']);
                     $idProject = $project[0]->getId();
                     Affectation::createAffectation($idProject);
                 } else {
@@ -51,73 +54,72 @@ class ProjectController {
             }
         }
         $view->render();
-    } 
+    }
 
-    public function displayProject() {
-        $view = new Views('DisplayProject','Project list');
+    public function displayProject()
+    {
+        $view = new Views('DisplayProject', 'Project list');
         if (Security::isConnected()) {
             $view->setVar('connected', true);
         } else {
             header('location: index.php');
         }
+
         $projects = Project::getByAttribute('idAdmin', $_SESSION['id']);
-        echo"<pre>";
-        var_dump($projects);
-        echo"</pre>";
         $user = User::getById($_SESSION['id']);
-        $user->setAffectation();
-        $projectsParticipant=[];
-        foreach ($user->getAffectations() as $affectation) {
-            echo"<pre>";
-            var_dump($affectation);
-            echo"</pre>";
+        $projectsMemberId = $this->displayOtherProjects($user);
+        $projectsParticipant = [];
+        foreach ($projectsMemberId as $projectid) {
+            $projectsParticipant[] = Project::getById($projectid);
         }
-        die();
-        $view->setVar('projects',$projects);
-        $view->setVar('projectsParticipant',$projectsParticipant);
+        $view->setVar('projects', $projects);
+        $view->setVar('projectsParticipant', $projectsParticipant);
         $view->render();
     }
 
-    private function isValid() {
+    private function isValid()
+    {
         $return = '';
         $return .= Validate::existingProject($_POST['projectName']);
         return $return;
     }
 
-    public function updateProject() {
-        $view = new Views('CreateProject','Update of project');
+    public function updateProject()
+    {
+        $view = new Views('CreateProject', 'Update of project');
         if (Security::isConnected()) {
             $view->setVar('connected', true);
         } else {
             header('location: index.php');
         }
-        $view->setVar('action','&update='.$_GET['update']);
+        $view->setVar('action', '&update=' . $_GET['update']);
         if (isset($_POST['create'])) {
-            if (($message=$this->isValid()) === '') {
+            if (($message = $this->isValid()) === '') {
                 if (Project::updateById()) {
                     $view->setVar('message', 'Project is updated');
                 } else {
                     $view->setVar('message', 'Project couldn\'t be updated');
                 }
             } else {
-                $view->setVar('message',$message);
+                $view->setVar('message', $message);
             }
         }
         $project = Project::getById($_GET['update']);
         $project->setTasks();
-        $view->setVar('id',$project->getId());
-        $view->setVar('projectName',$project->getProjectName());
-        $view->setVar('tasks',$project->getTasks());
+        $view->setVar('id', $project->getId());
+        $view->setVar('projectName', $project->getProjectName());
+        $view->setVar('tasks', $project->getTasks());
         $view->setVar('submit', 'Update');
         $view->render();
-}
+    }
 
-//a ameliorer, surement pas besoin de foreach (voire fonction deleteAffectationFromProject dans Model.php)
-    public function deleteProject(){
+    //a ameliorer, surement pas besoin de foreach (voire fonction deleteAffectationFromProject dans Model.php)
+    public function deleteProject()
+    {
         $project = Project::getById($_GET['delete']);
         $project->setAffectation();
         $affectations = $project->getAffectations();
-        if(count($affectations)!==0){
+        if (count($affectations) !== 0) {
             foreach ($affectations as $affectation) {
                 $array_affectation = (array) $affectation;
                 $id = array_values($array_affectation)[1];
@@ -125,8 +127,16 @@ class ProjectController {
             }
         }
         Project::deleteById($_GET['delete']);
-        
     }
 
-    
+    public function displayOtherProjects($user)
+    {
+        $user = User::getById($_SESSION['id']);
+        $user->setAffectation();
+        $return = [];
+        foreach ($user->getAffectations() as $affectation) {
+            $return[] = $affectation->getIdProject();
+        }
+        return $return;
+    }
 }
--- src/Views/DisplayTask.php
+++ src/Views/DisplayTask.php
@@ -1,7 +1,7 @@
 <?php
 
-echo "<h2>$pageTitle</h2>";
-echo "<h3>" . $project->getProjectName() . "</h3><br>";
+echo "<h2>" . $project->getProjectName() . "</h2><br>";
+echo "<h3>$pageTitle</h3>";
 $tasks = $project->getTasks();
 foreach ($tasks as $task) {
     echo $task->getTitle() . ' ';
@@ -12,16 +12,41 @@ foreach ($tasks as $task) {
         echo $task->getUser()->getFirstName() . ' ';
         echo $task->getUser()->getLastName() . ' ';
         echo $task->getUser()->getEmail();
-    } else {
+    } else if ($idAdmin == $_SESSION['id']) {
 ?>
-        <form method="POST" action="">
-            <input name='adduser' type='submit' value='Add User'>
-            <input name='createuser' type='submit' value='Create User'>
+        <form method="POST" action="" id="selectuser">
+            <input name='assignuser' type='submit' value='Assign User'>
         </form>
+        <select name="users_list" form="selectuser">
+            <option value=""></option>
+            <?php
+            foreach ($users as $user) {
+                echo '<option value="' . $user->getEmail() . '">' . $user->getEmail() . '</option>';
+            }
+            ?>
+        </select>
 <?php
     }
-
-    echo "<a href='index.php?page=". $_GET['page'] ."&idproject=" . $project->getId() . "&delete=" . $task->getId() . "'>Supprimer</a> ";
-    echo "<a href='index.php?page=" . $_GET['page'] . "&update=" . $task->getId() . "'>Modifier</a><br>";
+    if ($idAdmin == $_SESSION['id']) {
+        echo "<a href='index.php?page=" . $_GET['page'] . "&idproject=" . $project->getId() . "&delete=" . $task->getId() . "'>Supprimer</a> ";
+        echo "<a href='index.php?page=" . $_GET['page'] . "&update=" . $task->getId() . "'>Modifier</a><br>";
+    }
 }
-echo "<a href='index.php?page=" . $_GET['page'] . "&idproject=" . $project->getId() . "&insert=1'>Add new task</a>";
+if ($idAdmin == $_SESSION['id']) {
+    echo "<a href='index.php?page=" . $_GET['page'] . "&idproject=" . $project->getId() . "&insert=1'>Add new task</a>";
+}
+
+
+?>
+
+<ul>
+    <?php
+    foreach ($users as $user) {
+        echo "<li>" . $user->getFirstName() . $user->getLastName() . $user->getEmail() . "</li>";
+    }
+    ?>
+    <form method="POST" action="">
+        <input name='adduser' type='submit' value='Add User'>
+        <input name='createuser' type='submit' value='Create User'>
+    </form>
+</ul>
